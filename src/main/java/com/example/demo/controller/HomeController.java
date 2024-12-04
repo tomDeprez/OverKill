ebb97cf411147716ea1cc56acec6dc3409c2b951
Tom DEPREZ <tom_deprez@hotmail.fr>
2024-12-04 11:46:49 +0100

    ajouter commit auto

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        //System.out.println("HomeController.home");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        
         return "home";
     }
 } 
